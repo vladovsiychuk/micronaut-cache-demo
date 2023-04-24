88746ba468586e47cb73f50e8f91bd477e862ba3
Vladyslav Ovsiychuk <vladyslav.ovsiychuk@gmail.com>
2023-04-24 16:16:34 +0300

    prepare the example for caching

--- src/main/kotlin/com/example/controller/TestController.kt
+++ src/main/kotlin/com/example/controller/TestController.kt
@@ -4,12 +4,13 @@ import com.example.service.TestService
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.reactivex.Flowable
+import io.reactivex.Single
 
 @Controller("/test")
 class TestController (private val service: TestService) {
 
     @Get("/")
-    fun test() : Flowable<Map<String, String>> {
+    fun test() : Flowable<Pair<String,String>> {
         return service.test()
     }
 }
--- src/main/kotlin/com/example/service/TestService.kt
+++ src/main/kotlin/com/example/service/TestService.kt
@@ -3,25 +3,37 @@ package com.example.service
 import io.reactivex.Flowable
 import io.reactivex.Single
 import jakarta.inject.Singleton
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @Singleton
 class TestService {
 
-    fun test() : Flowable<Map<String, String>> {
-        return fluxOfListLetters()
-            .flatMap {
-                processLetters(it)
+    private val logger: Logger = LoggerFactory.getLogger(TestService::class.java)
+
+    fun test(): Flowable<Pair<String, String>> {
+        return flowableOfListLetters()
+            .flatMap { letters ->
+                processLetters(letters)
                     .toFlowable()
+                    .flatMap { processedLetters ->
+                        Flowable.fromIterable(processedLetters.entries)
+                    }
+                    .map { entry ->
+                        Pair(entry.key.uppercase(), entry.value)
+                    }
             }
     }
 
     private fun processLetters(letters: List<String>) : Single<Map<String, String>> {
+        logger.info("Inside processLetters")
+
         val letterMap = letters.associateWith { it.uppercase() }
 
         return Single.just(letterMap)
     }
 
-    private fun fluxOfListLetters(): Flowable<List<String>> {
-        return Flowable.fromIterable(listOf(listOf("a","b"), listOf("a","d")))
+    private fun flowableOfListLetters(): Flowable<List<String>> {
+        return Flowable.fromIterable(listOf(listOf("a", "b"), listOf("c", "d")))
     }
 }
